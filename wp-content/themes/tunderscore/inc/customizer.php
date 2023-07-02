d7456c496d4b9949e7d4318bfec51139242df0a3
taopaipaipai <tao.paipai9380@gmail.com>
2023-07-02 13:35:47 +0200

    Install du theme Enfant et Parent, Suppression theme non necessaire

---
+++ wp-content/themes/tunderscore/inc/customizer.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * TUnderScore Theme Customizer
+ *
+ * @package TUnderScore
+ */
+
+/**
+ * Add postMessage support for site title and description for the Theme Customizer.
+ *
+ * @param WP_Customize_Manager $wp_customize Theme Customizer object.
+ */
+function tunderscore_customize_register( $wp_customize ) {
+	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
+	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
+	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
+
+	if ( isset( $wp_customize->selective_refresh ) ) {
+		$wp_customize->selective_refresh->add_partial(
+			'blogname',
+			array(
+				'selector'        => '.site-title a',
+				'render_callback' => 'tunderscore_customize_partial_blogname',
+			)
+		);
+		$wp_customize->selective_refresh->add_partial(
+			'blogdescription',
+			array(
+				'selector'        => '.site-description',
+				'render_callback' => 'tunderscore_customize_partial_blogdescription',
+			)
+		);
+	}
+}
+add_action( 'customize_register', 'tunderscore_customize_register' );
+
+/**
+ * Render the site title for the selective refresh partial.
+ *
+ * @return void
+ */
+function tunderscore_customize_partial_blogname() {
+	bloginfo( 'name' );
+}
+
+/**
+ * Render the site tagline for the selective refresh partial.
+ *
+ * @return void
+ */
+function tunderscore_customize_partial_blogdescription() {
+	bloginfo( 'description' );
+}
+
+/**
+ * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
+ */
+function tunderscore_customize_preview_js() {
+	wp_enqueue_script( 'tunderscore-customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), _S_VERSION, true );
+}
+add_action( 'customize_preview_init', 'tunderscore_customize_preview_js' );

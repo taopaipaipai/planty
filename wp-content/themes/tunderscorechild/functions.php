f24088c1c66c19e67f82cde31b5b777562d9319f
taopaipaipai <tao.paipai9380@gmail.com>
2023-07-02 13:54:28 +0200

    Ajout du CSS personnalisé

--- wp-content/themes/tunderscorechild/functions.php
+++ wp-content/themes/tunderscorechild/functions.php
@@ -3,8 +3,10 @@
 function theme_enqueue_styles(){
     // Chargement du style.css du thème parent tunderscore
     wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
+    wp_enqueue_style('theme-style', get_stylesheet_directory_uri() . '/css/theme.css', array(), filemtime(get_stylesheet_directory() . '/css/theme.css'));
 
 }
 
 // Action qui permet de charger des scripts dans notre thème
 add_action('wp_enqueue_scripts', 'theme_enqueue_styles');
+
